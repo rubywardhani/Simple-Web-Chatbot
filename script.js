cd9bbe59b8190c997cfeb8f9a5125d8604acc166
Ruby Wardhani <hi.rubywardhani@gmail.com>
2025-05-29 18:00:42 +0700

    feat: add basic chatbot UI and message handling

---
+++ script.js
@@ -0,0 +1,25 @@
+function sendMessage() {
+    const userInput = document.getElementById("user-input");
+    const chatBox = document.getElementById("chat-box");
+
+    const message = userInput.ariaValueMax.trim();
+    if (message === "") return;
+
+    // View chat
+    chatBox.innerHTML += `<p><strong>You:</strong> ${message}</p>`;
+
+    // Reply simulation
+    const botReply = getBotReply(message);
+    chatBox.innerHTML += `<p><strong>Bot:</strong> ${botReply}</p>`;
+
+    // Auto scroll
+    chatBox.scrollTop = chatBox.scrollHeight;
+    userInput.value = "";
+}
+
+function getBotReply(msg) {
+    // Respond
+    if (msg.toLowerCase().includes("hello")) return "Hello there!";
+    if (msg.toLowerCase().includes("Who are you")) return "I'm your chatbot!";
+    return "Sorry, I don't understand what you mean";
+}
